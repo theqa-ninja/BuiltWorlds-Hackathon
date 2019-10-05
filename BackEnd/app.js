ff4380313739483ac8e1869eef292253ae8e0d16
daniel1yuan <daniel1yuan@gmail.com>
2019-10-05 07:36:36 -0700

    Setting up babel node and editor config for entire project

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,55 +1,56 @@
-const express = require('express');
-const ForgeSDK = require('forge-apis');
-const cookieSession = require('cookie-session')
+import express from 'express';
+import ForgeSDK from 'forge-apis';
+import cookieSession from 'cookie-session';
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const AUTODESK_CLIENT_ID = process.env.AUTODESK_CLIENT_ID
 const AUTODESK_CLIENT_SECRET = process.env.AUTODESK_CLIENT_SECRET
 const AUTODESK_REDIRECT_URL = process.env.AUTODESK_REDIRECT_URL
 
 app.use(cookieSession({
-    name: 'session',
-    keys: "blah",
-    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  name: 'session',
+  keys: "blah",
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
 
-app.get('/', (req, res) => {res.send("Hello")})
+app.get('/', (req, res) => { res.send("Hello") })
 
 app.get('/auth/signin', (req, res) => {
-    var autoRefresh = true;
-    res.redirect(302, oauthClient().generateAuthUrl());
+  var autoRefresh = true;
+  res.redirect(302, oauthClient().generateAuthUrl());
 })
 
 app.get('/auth/callback', (req, res) => {
-    req.query.code
-    oauthClient().getToken(req.query.code).then( (credentials) => {
+  req.query.code
+  oauthClient().getToken(req.query.code).then((credentials) => {
 
-        const UserApi = new ForgeSDK.UserProfileApi();
+    const UserApi = new ForgeSDK.UserProfileApi();
 
-        UserApi.getUserProfile(oauthClient(), credentials).then( (profile) => {
-            console.log("----------")
-            console.log(profile);
-            res.send("hello again!");
-        }).catch(e => {
-            console.error(e);
-            res.send("error pulling profile");
-        });
+    UserApi.getUserProfile(oauthClient(), credentials).then((profile) => {
+      console.log("----------")
+      console.log(profile);
+      res.send("hello again!");
     }).catch(e => {
-        console.error(e);
-        res.send("login failed");
+      console.error(e);
+      res.send("error pulling profile");
     });
+  }).catch(e => {
+    console.error(e);
+    res.send("login failed");
+  });
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 function oauthClient() {
-    return new ForgeSDK.AuthClientThreeLegged(
-        AUTODESK_CLIENT_ID,
-        AUTODESK_CLIENT_SECRET,
-        AUTODESK_REDIRECT_URL,
-        [
-            'data:read'
-        ],
-        true);
+  return new ForgeSDK.AuthClientThreeLegged(
+    AUTODESK_CLIENT_ID,
+    AUTODESK_CLIENT_SECRET,
+    AUTODESK_REDIRECT_URL,
+    [
+      'data:read'
+    ],
+    true);
 }
