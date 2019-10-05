e5802d22a1b03dfd75ebb8a7eedcca27a4ffce30
Sue Feng Tan <suefengtan@gmail.com>
2019-10-05 09:43:27 -0500

    routing test added

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -1,13 +1,22 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import routes from './routes'
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
+const app = new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent() {
+      const matchingView = routes[this.currentRoute]
+      return matchingView
+        ? require('./pages/' + matchingView + '.vue')
+        : require('./pages/404.vue')
+    }
+  },
+  template: '<main id="app" />'
+});
+
+window.addEventListener('popstate', () => {
+  app.currentRoute = window.location.pathname;
+});
