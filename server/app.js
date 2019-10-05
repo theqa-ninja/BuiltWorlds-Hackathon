879035eb3652d717ed18bf05d5f4694dd557ec62
Aaron Lee <aaron.lee@reconstructinc.com>
2019-10-05 09:57:41 -0500

    oauth part 2 - sessions

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const ForgeSDK = require('forge-apis');
-const cookieSession = require('cookie-session')
+const session = require('express-session')
 const app = express();
 const port = 3000;
 
@@ -8,36 +8,53 @@ const AUTODESK_CLIENT_ID = process.env.AUTODESK_CLIENT_ID
 const AUTODESK_CLIENT_SECRET = process.env.AUTODESK_CLIENT_SECRET
 const AUTODESK_REDIRECT_URL = process.env.AUTODESK_REDIRECT_URL
 
-app.use(cookieSession({
-    name: 'session',
-    keys: "blah",
-    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+app.set('trust proxy', 1);
+
+app.use(session({
+    secret: 'this was randomly selected',
+    resave: false,
+    saveUninitialized: true
 }));
+
+app.use(function (req, res, next) {
+    console.log('---middleware');
+
+    if(!req.session.credentials){
+        req.session.credentials = {}
+    }
+
+    next()
+});
 
 app.get('/', (req, res) => {res.send("Hello")})
 
 app.get('/auth/signin', (req, res) => {
-    var autoRefresh = true;
     res.redirect(302, oauthClient().generateAuthUrl());
 })
 
 app.get('/auth/callback', (req, res) => {
-    req.query.code
     oauthClient().getToken(req.query.code).then( (credentials) => {
+        console.log("--- 1 ---");
+        req.session.credentials['autodesk'] = credentials;
 
-        const UserApi = new ForgeSDK.UserProfileApi();
+        console.log(req.session.credentials);
 
-        UserApi.getUserProfile(oauthClient(), credentials).then( (profile) => {
-            console.log("----------")
-            console.log(profile);
-            res.send("hello again!");
-        }).catch(e => {
-            console.error(e);
-            res.send("error pulling profile");
-        });
+        res.redirect('/user_info');
     }).catch(e => {
         console.error(e);
         res.send("login failed");
+    });
+})
+
+app.get('/user_info', (req, res) => {
+    const UserApi = new ForgeSDK.UserProfileApi();
+    const credentials = req.session.credentials['autodesk'];
+
+    UserApi.getUserProfile(oauthClient(), credentials).then( (profile) => {
+        res.send("hello again!");
+    }).catch(e => {
+        console.error(e);
+        res.send("error pulling profile");
     });
 })
 
